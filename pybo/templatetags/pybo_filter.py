6fc6d38198a398ae9c1ccdec75cba19299622792
innateshy <innateshy@gmail.com>
2023-10-06 19:41:58 +0900

    파이보 프로젝트 최초 커밋

---
+++ pybo/templatetags/pybo_filter.py
@@ -0,0 +1,16 @@
+import markdown
+from django import template
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+@register.filter
+def sub(value, arg):
+    return value - arg
+
+@register.filter
+def mark(value):
+    extensions = ["nl2br", "fenced_code"]
+    # nl2br은 줄바꿈 문자를 <br>로 바꾸어 준다.
+    # fenced_code는 마크다운의 소스코드 표현을 위해 필요하다.
+    return mark_safe(markdown.markdown(value, extensions=extensions))
